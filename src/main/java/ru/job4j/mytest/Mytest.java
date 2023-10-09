aa3d4711735d67cd9de57ff021f7deec1c34e636
filipp.kucherov <filipp.kucerov@r1.team>
2023-10-09 17:28:15 +0700

    mytest commit

--- src/main/java/ru/job4j/mytest/Mytest.java
+++ src/main/java/ru/job4j/mytest/Mytest.java
@@ -1,5 +1,10 @@
 package ru.job4j.mytest;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeSet;
+
 public class Mytest {
 
     public static String binary(int num) {
@@ -13,6 +18,15 @@ public class Mytest {
     }
 
     public static void main(String[] args) {
-        System.out.println(binary(123));
+        TreeSet<Integer> set = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 0;
+            }
+        });
+        set.add(1);
+        set.add(2);
+        set.add(1);
+        System.out.println(set);
     }
 }
