1afb71992dc02a3f983b3bdad46ea784a6aaba50
filipp.kucherov <filipp.kucerov@r1.team>
2023-10-30 22:36:25 +0700

    Revert "2. Что такое Socket? [#4850] fix 3"
    This reverts commit a2b763b7de255ddb7990a40d475f63de17d15823.

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,14 +1,9 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -20,11 +15,8 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String str = in.readLine();
-                    Pattern pattern = Pattern.compile("msg=[^ ]+");
-                    Matcher matcher = pattern.matcher(str);
-                    if (matcher.find()) {
-                        List<String> params = List.of(matcher.group().split("="));
-                        if ("msg".contains(params.get(0)) && "Bye".contains(params.get(1))) {
+                    if (str != null && !str.isEmpty()) {
+                        if (str.contains("Bye") && str.contains("msg")) {
                             server.close();
                         } else {
                             System.out.println(str);
