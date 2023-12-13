ffca4d53f3a54d9b51ce711006514bdf64327870
filipp.kucherov <filipp.kucerov@r1.team>
2023-12-13 16:29:31 +0700

    1. Inner join [#373617] fix

--- db/devices.sql
+++ db/devices.sql
@@ -38,4 +38,8 @@ join people p on dp.people_id = p.id
 join devices d on dp.device_id = d.id
 group by p.name;
 
-select avg(price) from devices having avg(price)>5000.0;
+select p.name, avg(d.price) from devices_people dp
+join people p on dp.people_id = p.id
+join devices d on dp.device_id = d.id
+group by p.name;
+having avg(d.price) > 5000;
