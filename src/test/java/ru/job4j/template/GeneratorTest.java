252aed9b7ed1e10b669ab6e48e49f45049eb146f
filipp.kucherov <filipp.kucerov@r1.team>
2024-03-17 20:21:41 +0700

    4. Шаблонизатор. [#855] fix

--- src/test/java/ru/job4j/template/GeneratorTest.java
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -11,37 +11,35 @@ import static org.junit.jupiter.api.Assertions.*;
 @Disabled
 public class GeneratorTest {
 
-    public static final String template = "I am a ${name}, Who are ${subject}?";
+    public static final String TEMPLATE = "I am a ${name}, Who are ${subject}?";
 
     @Test
     public void getExceptedString() {
         String exceptedString = "I am a Filipp, Who are you?";
-        Generator generator = new Generator();
+        Generator generator = new TemplateGenerator();
         Map<String, String> args = new HashMap<>();
         args.put("name", "Filipp");
         args.put("subject", "you");
-        String resultStr = generator.produce(template, args);
+        String resultStr = generator.produce(TEMPLATE, args);
         assertEquals(exceptedString, resultStr);
     }
 
     @Test
     public void testProduceMissingKey() {
-        Generator generator = new Generator();
-        String template = "I am a ${name}, Who are ${subject}?";
+        Generator generator = new TemplateGenerator();
         Map<String, String> args = new HashMap<>();
         args.put("name", "Petr Arsentev");
-        assertThrows(IllegalArgumentException.class, () -> generator.produce(template, args));
+        assertThrows(IllegalArgumentException.class, () -> generator.produce(TEMPLATE, args));
     }
 
     @Test
     public void testProduceExtraKey() {
-        Generator generator = new Generator();
-        String template = "I am a ${name}, Who are ${subject}?";
+        Generator generator = new TemplateGenerator();
         Map<String, String> args = new HashMap<>();
         args.put("name", "Petr Arsentev");
         args.put("subject", "you");
         args.put("extra", "value");
-        assertThrows(IllegalArgumentException.class, () -> generator.produce(template, args));
+        assertThrows(IllegalArgumentException.class, () -> generator.produce(TEMPLATE, args));
 
     }
 }
