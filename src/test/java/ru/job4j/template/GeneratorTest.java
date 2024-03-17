f4f002b058ef5c9414894df41279541efa50aba8
filipp.kucherov <filipp.kucerov@r1.team>
2024-03-17 20:16:13 +0700

    4. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled
+public class GeneratorTest {
+
+    public static final String template = "I am a ${name}, Who are ${subject}?";
+
+    @Test
+    public void getExceptedString() {
+        String exceptedString = "I am a Filipp, Who are you?";
+        Generator generator = new Generator();
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Filipp");
+        args.put("subject", "you");
+        String resultStr = generator.produce(template, args);
+        assertEquals(exceptedString, resultStr);
+    }
+
+    @Test
+    public void testProduceMissingKey() {
+        Generator generator = new Generator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        assertThrows(IllegalArgumentException.class, () -> generator.produce(template, args));
+    }
+
+    @Test
+    public void testProduceExtraKey() {
+        Generator generator = new Generator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        args.put("extra", "value");
+        assertThrows(IllegalArgumentException.class, () -> generator.produce(template, args));
+
+    }
+}
