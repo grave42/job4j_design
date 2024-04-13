cd3b39537a9344af6cb35ead09196378b5838b27
filipp.kucherov <filipp.kucerov@r1.team>
2024-04-13 19:09:31 +0700

    3.1. Добавить изменения в последний коммит. [#504829] fix

---
+++ src/main/java/ru/job4j/TestGit.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        String testGit = "test Git";
+        String makeAmend = "Amend commit";
+    }
+}
