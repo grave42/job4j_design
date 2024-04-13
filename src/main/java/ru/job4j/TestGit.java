4d16edfeed30bfa5270b47743443dbadf5774029
filipp.kucherov <filipp.kucerov@r1.team>
2024-04-13 19:23:29 +0700

    Revert "3.1. Добавить изменения в последний коммит. [#504829] fix"
    This reverts commit cd3b39537a9344af6cb35ead09196378b5838b27.

--- src/main/java/ru/job4j/TestGit.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class TestGit {
-
-    public static void main(String[] args) {
-        String testGit = "test Git";
-        String makeAmend = "Amend commit";
-    }
-}
